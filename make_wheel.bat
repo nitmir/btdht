c2d15c57e080b725535dbb2a5e1bcd2eec10cff6
Valentin Samir <valentin.samir@crans.org>
2016-11-30 22:29:20 +0100

    Add a script to compile wheel for windows

---
+++ make_wheel.bat
@@ -0,0 +1,8 @@
+C:/Python27-32/Scripts/pip install wheel
+C:/Python27-32/python setup.py bdist_wheel
+C:/Python27/Scripts/pip install wheel
+C:/Python27/python setup.py bdist_wheel
+C:/Python35-32/python -m pip install wheel
+C:/Python35-32/python setup.py bdist_wheel
+C:/Python35/python -m pip install wheel
+C:/Python35/python setup.py bdist_wheel
